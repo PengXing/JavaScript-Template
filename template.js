2c28a35f54bf11a7be8a307cbc655e5dd7e7ac23
sekiyika <pengxing@baidu.com>
2013-04-11 21:04:16 +0800

    1. FIX: tpl method can't run under IE7 2. Add some docs

--- template.js
+++ template.js
@@ -1,20 +1,22 @@
-var app = {};
-
 /**
  * javascript template
  * 基于有限状态自动机的前端模板，非正则
  * 
- * @author PengXing
- * @email px.pengxing@gmail.com
+ * @author PengXing (px.pengxing@gmail.com)
  */
 ;(function(A){
 
+    /**
+     * Paser
+     * @constructor
+     * @param {string} str
+     */
     function Parser(str){
         var me = this;
         me.str = str;
         me.length = str.length;
 
-        var EOF = undefined;
+        var EOF = "";
 
         var START = 0,
             DONE = 1,
@@ -32,26 +34,19 @@ var app = {};
         me.offset = -1;
         me.lineno = 1;
 
-
-        /* 
-        too many function invokes slow down this program.
-        me.getNextChar = function(){
-            if(me.str[me.offset + 1] === undefined){
-                me.offset++;
-                return EOF;
-            }
-            return me.str[++me.offset];
-        };
-
-        me.ungetNextChar = function(){
-            me.offset--;
-        };
-        */
-
-        var error = function(msg){
+        /**
+         * Report error
+         * @param {string} msg The error message
+         */
+        function error (msg) {
             throw new Error(msg + ': lineno[' + me.lineno + ']');
         };
 
+        /**
+         * Get the next code statement
+         *
+         * @return {null|object} The statement object, it contains the start and end offset, ant the statement type.
+         */
         me.getNextStmt = function(){
             var status = START,
             start = me.offset + 1,
@@ -59,8 +54,7 @@ var app = {};
             category;
 
             while(status != DONE){
-                //var c = me.getNextChar();
-                var c = me.str[++me.offset];
+                var c = me.str.charAt(++me.offset);
 
                 if(c == "\n"){
                     me.str = me.str.substring(0, me.offset) + ' ' + me.str.substring(me.offset + 1);
@@ -70,8 +64,7 @@ var app = {};
                 switch(status){
                     case START:
                         if(c == '<'){
-                            //var _c = me.getNextChar();
-                            var _c = me.str[++me.offset];
+                            var _c = me.str.charAt(++me.offset);
                             if(_c == '%'){
                                 status = INCODE;
                             } else {
@@ -87,13 +80,11 @@ var app = {};
                         break;
                     case INCODE:
                         if(c == '%'){
-                            //var _c = me.getNextChar();
-                            var _c = me.str[++me.offset];
+                            var _c = me.str.charAt(++me.offset);
                             if(_c == '>'){
                                 status = DONE;
                                 category = type.CODE;
                             } else {
-                                //me.ungetNextChar();
                                 me.offset--;
                             }
                         } else if(c === EOF){
@@ -102,8 +93,7 @@ var app = {};
                         break;
                     case INTEXT:
                         if(c == '<'){
-                            //var _c = me.getNextChar();
-                            var _c = me.str[++me.offset];
+                            var _c = me.str.charAt(++me.offset);
                             if(_c == '%'){
                                 me.offset -= 2;
                                 status = DONE;
@@ -130,10 +120,11 @@ var app = {};
 
         me.parse = function(){
             var stmt,
-            code = '',
-            lastType = 1; //0代表直接输出，1代表其他
+                code = '',
+                lastType = 1;
             /**
-             * 拼接代码，对连续输出进行优化，两个连续的输出语句会合并为一条语句
+             * Combine two consecutive output statement to one line code.
+             * @type {string} str T
              */
             function concatCode(str, type){
                 switch(type){
@@ -156,34 +147,34 @@ var app = {};
             while(stmt = me.getNextStmt()){
                 switch(stmt.type){
                     case type.CODE:
-                        var flag = me.str[stmt.start + 2];
+                        var flag = me.str.charAt(stmt.start + 2);
                         if(flag == '='){
                             concatCode(me.str.substring(stmt.start + 3, stmt.end - 2), 0);
                         } else if(flag == ':'){
-                            flag = me.str[stmt.start + 3];
+                            flag = me.str.charAt(stmt.start + 3);
                             switch(flag){
                                 case 'u':
-                                    if(me.str[stmt.start + 4] == '='){
+                                    if(me.str.charAt(stmt.start + 4) == '='){
                                         concatCode('encodeURIComponent(' + me.str.substring(stmt.start + 5, stmt.end - 2) + ')', 0);
                                     } else 
                                         error("Errors. Expect char: \'=\'");
                                     break;
                                 case 'h':
-                                    if(me.str[stmt.start + 4] == '='){
+                                    if(me.str.charAt(stmt.start + 4) == '='){
                                         concatCode('encodeHTML(' + me.str.substring(stmt.start + 5, stmt.end - 2) + ')', 0);
                                     } else 
                                         error('Errors. Expect char: \'=\'');
                                     break;
                             }
                         } else if(flag == '#'){
-                            //注释
+                            // This is comment.
                             break;
                         } else {
                             concatCode(me.str.substring(stmt.start + 2, stmt.end - 2), 1);
                         }
                         break;
                     case type.TEXT:
-                        concatCode('"' + me.str.substring(stmt.start, stmt.end) + '"', 0);
+                        concatCode('"' + me.str.substring(stmt.start, stmt.end).replace(/"/g, "\\\"").replace(/'/g, "\\\'") + '"', 0);
                         break;
                     default:
                         error("Errors. Unsupported statement");
@@ -196,29 +187,25 @@ var app = {};
     }
 
     var innerFns = {
-        'encodeHTML' : [
-            'function(str){',
-                'return String(str)',
-                    '.replace(/&/g,"&amp;")',
-                    '.replace(/</g,"&lt;")',
-                    '.replace(/>/g,"&gt;")',
-                    '.replace(/"/g,"&quot;")',
-                    '.replace(/\'/g,"&#39;")',
-                    '.replace(/\\\\/g,"\\\\")',
-                    '.replace(/\\\//g,"\\\/")',
-            '}'
-        ].join(''),
-        'print' : 'function(s){__s+=s}'
+        'encodeHTML': function(str){
+            return String(str)
+                .replace(/&/g,"&amp;")
+                .replace(/</g,"&lt;")
+                .replace(/>/g,"&gt;")
+                .replace(/"/g,"&quot;")
+                .replace(/\'/g,"&#39;")
+                .replace(/\\\\/g,"\\\\")
+                .replace(/\\\//g,"\\\/")
+        },
+        'print': function(s){__s+=s}
     };
 
     /**
-     * @function 模板函数
-     * @param {string} 模板
-     * @param {object} 数据
-     * @param {object} 参数，可包含自定义函数
-     * @return {string|Function} 如果包含数据，则返回函数执行后的字符串，否则返回函数
+     * @param {!string} template The template string, can't be empty
+     * @param {object=} opt_data
+     * @param {opt=} opt
      */
-    var tpl = function(template, data, opt){
+    var tpl = function(template, opt_data, opt){
         if(!opt) opt = {};
         var parser = new Parser(template);
         var str = parser.parse();
@@ -228,7 +215,7 @@ var app = {};
         opt['fns'] = opt['fns'] || {};
         for(key in innerFns){
             value = innerFns[key];
-            opt['fns'][key] = opt['fns'][key] || value;
+            opt['fns'][key] = opt['fns'][key] || value.toString();
         }
         for(key in opt['fns']){
             value = opt['fns'][key];
@@ -236,8 +223,8 @@ var app = {};
             inFns += value;
         }
         var fn = new Function('obj', 'var __s="";' + inFns + 'with(obj){' + str + '}return __s');
-        return data ? fn(data) : fn;
+        return opt_data ? fn(opt_data) : fn;
     };
 
     A.tpl = tpl;
-})(app);
+})(window);
