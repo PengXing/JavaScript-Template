9950c5423e2a5de7ce0e207980a7eee08e1d9161
sekiyika <pengxing5501854@gmail.com>
2013-08-15 12:10:37 +0800

    格式调整 + 加注释
    ISSUE=IGNORE

--- template.js
+++ template.js
@@ -1,77 +1,92 @@
 /**
  * javascript template
- * 基于有限状态自动机的前端模板，非正则
+ * 基于有限状态自动机的前端模板，遍历一次模板字符串
  * 
  * @author PengXing (px.pengxing@gmail.com)
  */
-;(function(A){
+;(function(A) {
 
     /**
-     * Paser
+     * Paser 语法分析器
      * @constructor
-     * @param {string} str
+     * @param {string} str 模板字符串
      */
-    function Parser(str){
+    function Parser(str) {
+        /**
+         * 指向Parser实例
+         * @type {Parser}
+         */
         var me = this;
         me.str = str;
         me.length = str.length;
 
-        var EOF = "";
+        /**
+         * 结束符
+         * @type {string}
+         */
+        var EOF = '';
 
-        var START = 0,
-            DONE = 1,
-            INCODE = 2,
-            INCOMMENT = 3,
-            INTEXT = 4;
+        var START = 0; // 开始状态
+        var DONE = 1; // 结束状态
+        var INCODE = 2; // 在代码中
+        var INTEXT = 3; // 文本状态
 
+        /**
+         * 表示一个语句的类型
+         * @type {Object}
+         */
         var type = {
-            CODE : 0,
-            TEXT : 1,
-            NONE : 2
+            'CODE': 0,
+            'TEXT': 1,
+            'NONE': 2
         };
 
         //running variables
         me.offset = -1;
+        // 当前所在行号
         me.lineno = 1;
 
         /**
          * Report error
          * @param {string} msg The error message
          */
-        function error (msg) {
+        function error(msg) {
             throw new Error(msg + ': lineno[' + me.lineno + ']');
         };
 
         /**
          * Get the next code statement
          *
-         * @return {null|object} The statement object, it contains the start and end offset, ant the statement type.
+         * @return {null|{start:number, end:number, type:number}} The statement object, it contains the start and end offset, ant the statement type.
          */
-        me.getNextStmt = function(){
-            var status = START,
-            start = me.offset + 1,
-            end,
-            category;
+        me.getNextStmt = function() {
+            var status = START;
+            var start = me.offset + 1;
+            var end;
+            var category;
 
-            while(status != DONE){
+            while(status != DONE) {
                 var c = me.str.charAt(++me.offset);
 
-                if(c == "\n"){
-                    me.str = me.str.substring(0, me.offset) + ' ' + me.str.substring(me.offset + 1);
+                if(c === "\n"){
+                    me.str = ''
+                        + me.str.substring(0, me.offset)
+                        + ' '
+                        + me.str.substring(me.offset + 1);
                     c = ' ';
                     me.lineno++;
                 }
                 switch(status){
                     case START:
-                        if(c == '<'){
+                        if(c === '<') {
                             var _c = me.str.charAt(++me.offset);
-                            if(_c == '%'){
+                            if(_c === '%'){
                                 status = INCODE;
                             } else {
                                 me.offset--;
                                 status = INTEXT;
                             }
-                        } else if(c === EOF){
+                        } else if(c === EOF) {
                             status = DONE;
                             category = type.NONE;
                         } else {
@@ -79,22 +94,22 @@
                         }
                         break;
                     case INCODE:
-                        if(c == '%'){
+                        if(c === '%') {
                             var _c = me.str.charAt(++me.offset);
-                            if(_c == '>'){
+                            if(_c === '>'){
                                 status = DONE;
                                 category = type.CODE;
                             } else {
                                 me.offset--;
                             }
                         } else if(c === EOF){
-                            error("Error. There is a single syntax seperator.");
+                            error('Error. There is a single syntax seperator.');
                         }
                         break;
                     case INTEXT:
-                        if(c == '<'){
+                        if(c === '<'){
                             var _c = me.str.charAt(++me.offset);
-                            if(_c == '%'){
+                            if(_c === '%'){
                                 me.offset -= 2;
                                 status = DONE;
                                 category = type.TEXT;
@@ -107,29 +122,34 @@
 
             }
 
-            if(category == type.NONE){
+            if(category === type.NONE){
                 return null;
             } else {
-                return {
+                return ({
                     start : start,
                     end : me.offset + 1,
                     type : category
-                };
+                });
             }
         };
 
-        me.parse = function(){
-            var stmt,
-                code = '',
-                lastType = 1;
+        /**
+         * 处理模板，生成拼接好的字符串
+         * @return {string}
+         */
+        me.parse = function() {
+            var code = '';
+            var lastType = 1;
+
             /**
              * Combine two consecutive output statement to one line code.
-             * @type {string} str T
+             * @param {string} str
+             * @param {number} type
              */
-            function concatCode(str, type){
+            function concatCode(str, type) {
                 switch(type){
                     case 0 :
-                        if(lastType){
+                        if(lastType) {
                             code += '__s+=' + str;
                         } else {
                             code += '+' + str;
@@ -137,47 +157,74 @@
                         lastType = 0;
                         break;
                     default:
-                        if(!lastType){
+                        if(!lastType) {
                             code += ';'
                         }
                         code += str;
                         lastType = 1;
                 }
             };
-            while(stmt = me.getNextStmt()){
-                switch(stmt.type){
+
+            var stmt;
+            while(stmt = me.getNextStmt()) {
+                switch(stmt.type) {
                     case type.CODE:
                         var flag = me.str.charAt(stmt.start + 2);
-                        if(flag == '='){
-                            concatCode(me.str.substring(stmt.start + 3, stmt.end - 2), 0);
+                        if(flag === '=') {
+                            concatCode(
+                                me.str.substring(stmt.start + 3, stmt.end - 2),
+                                0
+                            );
                         } else if(flag == ':'){
                             flag = me.str.charAt(stmt.start + 3);
                             switch(flag){
                                 case 'u':
                                     if(me.str.charAt(stmt.start + 4) == '='){
-                                        concatCode('encodeURIComponent(' + me.str.substring(stmt.start + 5, stmt.end - 2) + ')', 0);
+                                        concatCode(
+                                            'encodeURIComponent('
+                                                + me.str.substring(
+                                                    stmt.start + 5, stmt.end - 2
+                                                )
+                                                + ')',
+                                            0
+                                        );
                                     } else 
-                                        error("Errors. Expect char: \'=\'");
+                                        error('Errors. Expect char: \'=\'');
                                     break;
                                 case 'h':
                                     if(me.str.charAt(stmt.start + 4) == '='){
-                                        concatCode('encodeHTML(' + me.str.substring(stmt.start + 5, stmt.end - 2) + ')', 0);
+                                        concatCode(
+                                            'encodeHTML('
+                                                + me.str.substring(
+                                                    stmt.start + 5, stmt.end - 2
+                                                )
+                                                + ')',
+                                            0
+                                        );
                                     } else 
                                         error('Errors. Expect char: \'=\'');
                                     break;
                             }
-                        } else if(flag == '#'){
-                            // This is comment.
+                        } else if(flag === '#'){
                             break;
                         } else {
-                            concatCode(me.str.substring(stmt.start + 2, stmt.end - 2), 1);
+                            concatCode(
+                                me.str.substring(stmt.start + 2, stmt.end - 2),
+                                1
+                            );
                         }
                         break;
                     case type.TEXT:
-                        concatCode('"' + me.str.substring(stmt.start, stmt.end).replace(/"/g, "\\\"").replace(/'/g, "\\\'") + '"', 0);
+                        concatCode(
+                            '"'
+                                + me.str.substring(stmt.start, stmt.end)
+                                    .replace(/"/g, "\\\"").replace(/'/g, "\\\'")
+                                + '"',
+                            0
+                        );
                         break;
                     default:
-                        error("Errors. Unsupported statement");
+                        error('Errors. Unsupported statement');
                 }
             }
 
@@ -186,8 +233,12 @@
 
     }
 
+    /**
+     * 模板内置函数
+     * @type {{encodeHTML:Function, print: Function}}
+     */
     var innerFns = {
-        'encodeHTML': function(str){
+        'encodeHTML': function(str) {
             return String(str)
                 .replace(/&/g,"&amp;")
                 .replace(/</g,"&lt;")
@@ -197,32 +248,48 @@
                 .replace(/\\\\/g,"\\\\")
                 .replace(/\\\//g,"\\\/")
         },
-        'print': function(s){__s+=s}
+        'print': function(s) {__s+=s;}
     };
 
     /**
+     * 模板主函数，解析模板并且根据数据渲染出string或者返回模板函数
      * @param {!string} template The template string, can't be empty
-     * @param {object=} opt_data
-     * @param {opt=} opt
+     * @param {Object=} opt_data
+     * @param {{fns:Object.<string, Function>}=} opt 可选项，目前只接受fns函数
+     *
+     * @return {string|Function} 如果opt_data不为空，则返回渲染之后的string，反之，返回编译好的Function
      */
-    var tpl = function(template, opt_data, opt){
-        if(!opt) opt = {};
-        var parser = new Parser(template);
-        var str = parser.parse();
-        var inFns = '',
-            key,
-            value;
-        opt['fns'] = opt['fns'] || {};
-        for(key in innerFns){
+    var tpl = function(template, opt_data, opt) {
+        if(!opt){
+            opt = {
+                'fns': {}
+            };
+        };
+
+        var key;
+        var value;
+        for(key in innerFns) {
             value = innerFns[key];
             opt['fns'][key] = opt['fns'][key] || value.toString();
         }
-        for(key in opt['fns']){
+
+        var inFns = '';
+        for(key in opt['fns']) {
             value = opt['fns'][key];
-            value = 'var ' + key + '=' + value.replace(/[\r\t\n]/g, ' ') + ';';
+            value = 'var ' 
+                + key
+                + '='
+                + value.replace(/[\r\t\n]/g, ' ') 
+                + ';';
             inFns += value;
         }
-        var fn = new Function('obj', 'var __s="";' + inFns + 'with(obj){' + str + '}return __s');
+
+        var parser = new Parser(template);
+        var str = parser.parse();
+        var fn = new Function (
+            'obj',
+            'var __s="";' + inFns + 'with(obj){' + str + '}return __s'
+        );
         return opt_data ? fn(opt_data) : fn;
     };
 
